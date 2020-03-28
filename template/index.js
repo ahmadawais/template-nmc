b520494e77267d9c50fc40f7f990f380a039a981
Ahmad Awais <me@AhmadAwais.com>
2020-03-29 02:05:45 +0500

    🐛 FIX: CLI

--- template/index.js
+++ template/index.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
 
 const ora = require('ora');
-const spinner = ora({ text: '' });
+const spinner = ora({text: ''});
 const cli = require('./utils/cli.js');
 const init = require('./utils/init.js');
 const theEnd = require('./utils/theEnd.js');
 const handleError = require('cli-handle-error');
 
+// CLI.
+const [input] = cli.input;
+const option = cli.flags.option;
+
 module.exports = async () => {
 	init();
-	const [input] = cli.input;
-	const option = cli.flags.option;
 	theEnd();
 };
