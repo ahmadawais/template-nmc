119d504c6e99bc31e872e86461fffa9797cc320e
Ahmad Awais <me@AhmadAwais.com>
2020-03-29 01:33:22 +0500

    📦 NEW: First version

---
+++ template/init.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Create the repo with name and description.
+gh re --new "{{name}}" --description "{{description}}"
+
+# Git stuff.
+git init
+git remote add origin https://github.com/ahmadawais/{{name}}.git
+
+# npm stuff.
+npm install ora meow chalk cli-welcome cli-check-node cli-handle-unhandled
+npm install -D ava execa
+
+# Delete yourself, bash script.
+rm -- "$0"
