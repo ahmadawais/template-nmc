3cd2126ec0f63e3a963a213c7712dfcb0a4980f6
Ahmad Awais <me@AhmadAwais.com>
2020-03-29 17:50:38 +0500

    🐛 FIX: update-notifier

--- template/init.sh
+++ template/init.sh
@@ -8,7 +8,7 @@ git init
 git remote add origin https://github.com/ahmadawais/{{name}}.git
 
 # npm stuff.
-npm install ora meow chalk cli-welcome cli-check-node cli-handle-unhandled cli-handle-error await-to-js
+npm install ora meow chalk cli-welcome cli-check-node cli-handle-unhandled cli-handle-error await-to-js update-notifier
 npm install -D ava execa prettier
 
 # Delete yourself, bash script.
