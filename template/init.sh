1287a27a883f36b8d07c29f5395f502ebe97a4fa
Ahmad Awais ⚡️ <me@AhmadAwais.com>
2023-06-17 17:26:00 +0500

    📦 NEW: Git repo

--- template/init.sh
+++ template/init.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Set up a git repo.
+git init &>/dev/null
+gh repo create --description "description" --homepage "https://twitter.com/MrAhmadAwais" --public --source=. &>/dev/null
+
 # npm stuff.
 npm install ora meow chalk cli-welcome cli-check-node cli-handle-unhandled cli-handle-error await-to-js update-notifier
 npm install -D ava execa prettier
