23cfd9fd79f3af4ea2ae2f45231db5b62df18264
Ahmad Awais ⚡️ <me@AhmadAwais.com>
2023-06-17 17:01:28 +0500

    👌 IMPROVE: No git stuff

--- template/init.sh
+++ template/init.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-# Create the repo with name and description.
-gh re --new "{{name}}" --description "{{description}}" --homepage "https://twitter.com/MrAhmadAwais/"
-
-# Git stuff.
-git init
-git remote add origin https://github.com/ahmadawais/{{name}}.git
-
 # npm stuff.
 npm install ora meow chalk cli-welcome cli-check-node cli-handle-unhandled cli-handle-error await-to-js update-notifier
 npm install -D ava execa prettier
